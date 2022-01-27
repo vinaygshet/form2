353cfa4441cb0578e9a6c071d831051a6995f0b1
vinay g shet <vinaygshet4@gmail.com>
2022-01-28 03:21:53 +0530

    first commit of my Day-3 repository

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-var res ="hello"
-console.log(res)
+//For the given JSON iterate over all for loops (for, for in, for of, forEach)
+
+let object=[{
+    "name":"ramu",
+    "age":"25",
+    "city":"Banglore",
+    "state":"Karnataka",
+    "country":"india",
+    "role":"software developer",
+    "company":"Tech soft"
+}]
+
+//for loop
+
+for(var i=0;i<object.length;i++){
+    console.log(object[i])
+}
+//for in loop
+
+for(var i in object){
+    console.log(object[i])
+}
+
+//for of
+for(var k of object){
+    console.log(k)
+}
+//forEach
+var res=object.forEach((ele)=> console.log(ele))
---
+++ script2.js
@@ -0,0 +1,17 @@
+//Create your own resume data in JSON format
+let myresume=[{
+    "Name":"Vinay G shet",
+    "DOB":`27.06.1998`,
+    "Email":"Vinaygshet4@gmail.com",
+    "Education":"BCA",
+    "softwareLanguageKnown":["JAVASCRIPT","HTML","CSS","REACT.JS","NODE.JS","SQL","MONGODB"],
+    "Experience":"Fresher",
+    "LanguageKnown":["Hindi","Kannada","Konkani","Marathi","English"],
+    "Nationality":"Indian",
+    "Hobies":["cricket","football","reading","photography","travelling"],
+    "Martial Status":"Single"
+
+
+
+
+}]
